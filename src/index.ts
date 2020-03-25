c05ec57a2bf998b80f5304fe706a8d7b747f9131
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-25 11:26:15 +0800

    feat(user): Configure passport to use passport-local strategy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { AppModule } from "./app.module";
 import { MayaJS } from "@mayajs/core";
 import { configurations } from "./configurations";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import User from "./user/user.model";
 
 const server = new MayaJS(AppModule);
+server.use(() => {
+   passport.use(
+      new Strategy(
+         {
+            usernameField: "email",
+            passwordField: "password",
+         },
+         User.authenticate()
+      )
+   );
+   passport.serializeUser(User.serializeUser());
+   passport.deserializeUser(User.deserializeUser());
+});
 server.prodMode(configurations.production).start(configurations.APP_PORT as number);
