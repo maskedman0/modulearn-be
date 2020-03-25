8d1289f72774ac9e4f77f78b929efde6ad1de89c
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-25 14:38:41 +0800

    refactor(): use passport config function in server initialization

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
 import { AppModule } from "./app.module";
 import { MayaJS } from "@mayajs/core";
-import { configurations } from "./configurations";
-import passport from "passport";
-import { Strategy } from "passport-local";
-import User from "./user/user.model";
+import { configurations, configurePassport } from "./configurations";
 
 const server = new MayaJS(AppModule);
-server.use(() => {
-   passport.use(
-      new Strategy(
-         {
-            usernameField: "email",
-            passwordField: "password",
-         },
-         User.authenticate()
-      )
-   );
-   passport.serializeUser(User.serializeUser());
-   passport.deserializeUser(User.deserializeUser());
-});
+server.use(configurePassport);
 server.prodMode(configurations.production).start(configurations.APP_PORT as number);
