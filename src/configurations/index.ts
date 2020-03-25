5668c633ea731e7bacf42ec3e9d2b1a9220ea3f0
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-25 09:52:22 +0800

    refactor(): get node_env variable to determine production environment

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const configurations = {
-   production: false,
+   production: process.env.NODE_ENV === "production" ? true : false,
    MONGO_URL: process.env.MONGO_URL || "",
    APP_PORT: process.env.APP_PORT || 3333,
    API_ROOT: process.env.API_ROOT || "/api",
