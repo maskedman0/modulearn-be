4dbbd7e37c4cbde457b89c8217907cd417cb329c
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-25 11:27:01 +0800

    refactor(user): use passport-local-mongoose in user-model

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,5 +1,5 @@
 import { MongoSchema, MongoModel } from "@mayajs/mongo";
-
+import passportLocalMongoose from "passport-local-mongoose";
 const userSchema = MongoSchema(
    {
       firstName: {
@@ -27,5 +27,12 @@ const userSchema = MongoSchema(
    },
    { timestamps: true }
 );
+
+const passportLocalMongooseOptions = {
+   limitAttempts: true,
+   maxAttempts: 5,
+};
+
+userSchema.plugin(passportLocalMongoose, passportLocalMongooseOptions);
 
 export default MongoModel("User", userSchema);
