204d15e940320c59699a6acf83d2e623edb27326
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-26 22:31:09 +0800

    refactor(): enabled authguard and error handler middleware

--- src/app.routing.module.ts
+++ src/app.routing.module.ts
@@ -1,11 +1,12 @@
 import { GuideController } from "./guide/guide.controller";
 import { configurations } from "./configurations";
 import { UserController } from "./user/user.controller";
+import { authGuard, errorHandler } from "./shared/middlewares/auth.middleware";
 
 export const routes = [
    {
       controllers: [GuideController, UserController],
-      middlewares: [],
+      middlewares: [authGuard, errorHandler],
       path: `${configurations.API_ROOT}/${configurations.API_VERSION}`,
    },
 ];
