71690fa91bac34b357e6c69d2d1bc56979725ca3
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-26 22:27:00 +0800

    feat(): added auth-guard

--- src/shared/middlewares/auth.middleware.ts
+++ src/shared/middlewares/auth.middleware.ts
@@ -1,5 +1,30 @@
 import { authenticate } from "passport";
+import jwt from "express-jwt";
+import { config } from "dotenv";
+import returnError from "../functions/return-error.function";
+import { Request, Response, NextFunction } from "express";
+
+config();
+
+const root = `${process.env.API_ROOT}/${process.env.API_VERSION}`;
+const unvalidatedPaths = [
+   { url: `${root}/users`, methods: ["POST"] },
+   { url: `${root}/users/admin/login`, methods: ["POST"] },
+   { url: `${root}/users/reader/login`, methods: ["POST"] },
+   { url: `${root}/users/publisher/login`, methods: ["POST"] },
+];
 
 const localAuth = authenticate("local", { session: false });
+const authGuard = jwt({ secret: process.env.API_SECRET }).unless({ path: unvalidatedPaths });
 
-export { localAuth };
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+   returnError({
+      res,
+      status: err.status || 500,
+      message: err.message || "Server Error",
+      data: { error: err.message || err },
+   });
+   return;
+};
+
+export { localAuth, authGuard, errorHandler };
