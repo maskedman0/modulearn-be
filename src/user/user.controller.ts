6ba8d020acf50e6333de4c059001f07c117927ca
Jayson San Agustin <santiagomvp72F@gmail.com>
2020-03-26 13:10:40 +0800

    feat(user): added login function

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
 import { Get, Post, Delete, Put } from "@mayajs/common";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { Controller } from "@mayajs/core";
-import { CREATED, UNPROCESSABLE_ENTITY } from "http-status";
+import { CREATED, UNPROCESSABLE_ENTITY, OK, FORBIDDEN } from "http-status";
 import { UserService } from "./user.service";
 import { IUser } from "./user";
 import returnError from "../shared/functions/return-error.function";
 import returnSuccess from "../shared/functions/return-success.function";
+import { localAuth } from "../shared/middlewares/auth.middleware";
 
 @Controller({
    model: "./user.model",
@@ -23,6 +24,19 @@ export class UserController {
 
       if (user) {
          returnSuccess({ res, status: CREATED, data: user });
+      }
+   }
+
+   @Post({
+      path: "/login",
+      middlewares: [localAuth],
+   })
+   async login(req: Request, res: Response) {
+      const userToken = await this.userService.generatePublisherToken(req.body.email);
+      if (userToken.token) {
+         returnSuccess({ res, status: OK, message: `Welcome back ${userToken.user.firstName}`, data: { token: userToken.token } });
+      } else {
+         returnError({ res, status: FORBIDDEN, data: { error: { message: "Invalid Credentials" } } });
       }
    }
 }
